ba7de88267656f4e330cf14b082564466248a1dd
FireLord1 <firelord.xda@gmail.com>
2017-07-28 23:44:15 +0530

    kuntao: Fix wcnss_service
    wcnss_service: Failed to open libwcnss_qmi.so: dlopen failed: library "libwcnss_qmi.so" not found
    
    Signed-off-by: FireLord1 <firelord.xda@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -151,6 +151,7 @@ include device/qcom/sepolicy/sepolicy.mk
 # WiFi
 BOARD_HAS_QCOM_WLAN := true
 BOARD_HAS_QCOM_WLAN_SDK := true
+TARGET_USES_QCOM_WCNSS_QMI := true
 BOARD_HOSTAPD_DRIVER := NL80211
 BOARD_HOSTAPD_PRIVATE_LIB := lib_driver_cmd_qcwcn
 BOARD_WLAN_DEVICE := qcwcn
