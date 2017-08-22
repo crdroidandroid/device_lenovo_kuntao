c95aa99d3d7ada6d9a287a3287cba2a1bd7208aa
Luca Stefani <luca020400@lineageos.org>
2017-08-22 22:23:30 +0200

    msm8996-common: Add HIDL manifest
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- device.mk
+++ device.mk
@@ -168,6 +168,9 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/gps/etc/sap.conf:system/etc/sap.conf \
     $(LOCAL_PATH)/gps/etc/xtwifi.conf:system/etc/xtwifi.conf \
 
+# HIDL
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/manifest.xml:system/vendor/manifest.xml
 
 # IPA Manager
 PRODUCT_PACKAGES += \
