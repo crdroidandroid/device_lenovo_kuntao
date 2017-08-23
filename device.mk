8960d58fba8244f766a3ac9bf8d5aa34428da612
Luca Stefani <luca020400@lineageos.org>
2017-08-23 16:40:39 +0200

    kuntao: Add seccomp policy
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- device.mk
+++ device.mk
@@ -270,6 +270,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Seccomp policy
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:system/vendor/etc/seccomp_policy/mediacodec.policy
+
 # Sensor
 PRODUCT_PACKAGES += \
     android.hardware.sensors@1.0-impl \
