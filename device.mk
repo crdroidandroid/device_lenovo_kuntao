14e5416539c217edd5309691a6faa05a13a7e38e
Luca Stefani <luca020400@lineageos.org>
2017-09-26 16:57:00 +0530

    kuntao: Build Gatekeeper HAL
    Signed-off-by: Subhaam <derksoul98@gmail.com>
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- device.mk
+++ device.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fs_config_files
 
+# Gatekeeper HAL
+PRODUCT_PACKAGES += \
+    android.hardware.gatekeeper@1.0-impl
+
 # GPS
 PRODUCT_PACKAGES += \
     libgnsspps \
