ff3f1ed50ffd049fabbc12bd7a80ec1203cc48d7
Luca Stefani <luca020400@lineageos.org>
2017-09-27 02:31:10 +0530

    kuntao: Build Vibrator HAL
    Signed-off-by: FireLord1 <firelord.xda@lineageos.org>

--- device.mk
+++ device.mk
@@ -265,6 +265,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/hals.conf:system/etc/sensors/hals.conf
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    android.hardware.vibrator@1.0-impl
+
 # WiFi HAL
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service
