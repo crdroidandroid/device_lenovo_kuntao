27e6bb585240ceb401e52b08fccf8e341e997a0c
Vitaliy Tomin <highwaystar.ru@gmail.com>
2017-10-29 12:31:11 +0800

    kuntao: Enable build of OSS camera HAL
    * Drop proprietary camera HAL files
    * Adjust camera flags
    
    Change-Id: I5bf03fd6134321fd7c619af3fe4927019f12f118
    Signed-off-by: Subhaam <derksoul98@gmail.com>

--- device.mk
+++ device.mk
@@ -86,6 +86,7 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     Snap \
+    camera.msm8953 \
     libshim_camera \
     camera.device@3.2-impl \
     android.hardware.camera.provider@2.4-impl
