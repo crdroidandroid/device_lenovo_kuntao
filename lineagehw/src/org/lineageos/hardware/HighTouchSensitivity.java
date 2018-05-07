LineageOS/android_device_lenovo_kuntao

kuntao: Enable glove mode
Change-Id: I70212ee097798fbb0256ea04abc6935ebfe63682

cmhw/src/org/cyanogenmod/hardware/HighTouchSensitivity.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.cyanogenmod.hardware;
+
+import org.cyanogenmod.internal.util.FileUtils;
+
+/**
+ * Glove mode / high touch sensitivity
+ */
+public class HighTouchSensitivity {
+    private static final String CONTROL_PATH = "/sys/board_properties/tpd_glove_status";
+
+    /**
+     * Whether device supports high touch sensitivity.
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return FileUtils.isFileReadable(CONTROL_PATH) &&
+                FileUtils.isFileWritable(CONTROL_PATH);
+    }
+
+    /** This method returns the current activation status of high touch sensitivity
+     *
+     * @return boolean Must be false if high touch sensitivity is not supported or not activated,
+     * or the operation failed while reading the status; true in any other case.
+     */
+    public static boolean isEnabled() {
+        return FileUtils.readOneLine(CONTROL_PATH).equals("1");
+    }
+
+    /**
+     * This method allows to setup high touch sensitivity status.
+     *
+     * @param state The new high touch sensitivity status
+     * @return boolean Must be false if high touch sensitivity is not supported or the operation
+     * failed; true in any other case.
+     */
+    public static boolean setEnabled(boolean state) {
+        return FileUtils.writeLine(CONTROL_PATH, (state ? "1" : "0"));
+    }
+}

crdroidandroid/device_lenovo_kuntao

Signed-off-by: FireLord1 <firelord.xda@lineageos.org>
Signed-off-by: Subhaam <derksoul98@gmail.com>

lineagehw/src/org/lineageos/hardware/HighTouchSensitivity.java
@@ -1,0 +1,56 @@
+/*
+ * Copyright (C) 2016 The CyanogenMod Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.lineageos.hardware;
+
+import org.lineageos.internal.util.FileUtils;
+
+/**
+ * Glove mode / high touch sensitivity
+ */
+public class HighTouchSensitivity {
+    private static final String CONTROL_PATH = "/sys/board_properties/tpd_glove_status";
+
+    /**
+     * Whether device supports high touch sensitivity.
+     *
+     * @return boolean Supported devices must return always true
+     */
+    public static boolean isSupported() {
+        return FileUtils.isFileReadable(CONTROL_PATH) &&
+                FileUtils.isFileWritable(CONTROL_PATH);
+    }
+
+    /** This method returns the current activation status of high touch sensitivity
+     *
+     * @return boolean Must be false if high touch sensitivity is not supported or not activated,
+     * or the operation failed while reading the status; true in any other case.
+     */
+    public static boolean isEnabled() {
+        return FileUtils.readOneLine(CONTROL_PATH).equals("1");
+    }
+
+    /**
+     * This method allows to setup high touch sensitivity status.
+     *
+     * @param state The new high touch sensitivity status
+     * @return boolean Must be false if high touch sensitivity is not supported or the operation
+     * failed; true in any other case.
+     */
+    public static boolean setEnabled(boolean state) {
+        return FileUtils.writeLine(CONTROL_PATH, (state ? "1" : "0"));
+    }
+}